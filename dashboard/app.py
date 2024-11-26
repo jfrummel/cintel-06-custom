beee341e746a9f67ae5e4dcc6a5cb1d94d92210d
Jeremy Rummel <jfrummel@gmail.com>
2024-11-26 13:18:45 -0600

    Added csv and data frame

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,9 @@
+import pandas as pd
+from shiny.express import input, ui
+from shiny import render
+df = pd.read_csv("careers.csv")
 
+ui.page_opts()
+@render.data_frame  
+def careers_df():
+    return render.DataGrid(df) 
