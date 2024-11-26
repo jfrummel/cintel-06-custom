91d25a3f5d5ed3d090b3d1d5b8af2e32fe54a911
Jeremy Rummel <42078577+jfrummel@users.noreply.github.com>
2024-11-26 14:48:24 -0600

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from shiny.express import input, ui
 from shiny import render
+from shared import app_dir, careers_df, from_start, gp_max, players_dict, stats, to_end
 df = pd.read_csv("careers.csv")
 
 ui.page_opts()
