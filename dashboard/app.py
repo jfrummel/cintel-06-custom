db624ff894424d07e2b504687d7651f625c197f3
Jeremy Rummel <42078577+jfrummel@users.noreply.github.com>
2024-11-26 19:15:03 -0600

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from shiny.express import input, ui
 from shiny import render
-from shared import app_dir, careers_df, from_start, gp_max, players_dict, stats, to_end
-df = pd.read_csv("careers.csv")
+df = pd.read_csv("Premier League Player Stats.csv")
 
 ui.page_opts()
 @render.data_frame  
-def careers_df():
+def stats_df():
     return render.DataGrid(df) 
