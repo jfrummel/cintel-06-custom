dafd3c42e3e5c5a7a9dbf54121c2158f6208e0e8
Jeremy Rummel <jfrummel@gmail.com>
2024-11-29 16:20:12 -0600

    Added reset button

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,13 +4,14 @@ from shiny import render, reactive, req
 from shinywidgets import render_widget  
 import plotly.express as px
 from shinyswatch import theme
+from faicons import icon_svg
 
 df = pd.read_csv("premier-player-23-24.csv")
 stats_df = df[["Player", "Pos", "Age", "MP", "Gls", "Ast", "Team"]]
 goal_range = [float(df["Gls"].min()),float(df["Gls"].max())]
 players = stats_df["Player"].tolist()
 
-ui.page_opts(title="Premier League Player Stats 2023-24", fillable=True, theme=theme.darkly)
+ui.page_opts(title="Premier League Player Stats 2023-24",window_title="Jeremy's Interactive App", fillable=True, theme=theme.darkly)
 with ui.sidebar(position="left", open="open", bg="f8f8f8"):
     ui.h2("Compare Players", style="text-align: center;", bg="#0a0a0a")
     
@@ -30,6 +31,9 @@ with ui.sidebar(position="left", open="open", bg="f8f8f8"):
         selected=None
     )
 
+
+    ui.input_action_button("reset_button", "Reset")  
+
 with ui.layout_columns():
     with ui.card(full_screen=True):
         ui.card_header("Player Table", style="text-align: center;")
@@ -45,7 +49,7 @@ with ui.card(full_screen=True):
         def plotly_bar():
             stat=input.select_stat()
             compare_df= filtered_data()
-            chart = px.histogram(
+            chart = px.bar(
                 compare_df, x=stat, y="Player", orientation='h', hover_data=input.select_stat()
             ).update_layout(
                 xaxis_title=f"{stat}",
@@ -63,3 +67,8 @@ def filtered_data():
     isPlayerMatch = stats_df["Player"].isin(input.select_player())
     selectStat = stats_df[input.select_stat()]
     return stats_df[isPlayerMatch & selectStat]
+
+@reactive.effect
+@reactive.event(input.reset_button)
+def reset():
+    ui.update_select("select_player", selected=[])
